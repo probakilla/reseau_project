75529af2ebbd99231f0b75327254a579ded8c6c2
julpilleux <julien.pilleux@etu.u-bordeaux.fr>
2017-01-04 12:11:44 +0100

    Premiere version du client (a voir ensemble pour modifications)

--- projet/client.py
+++ projet/client.py
@@ -3,7 +3,16 @@
 from grid import *
 import  random, socket, select, threading, sys
 
+print ("Bienvenue dans le jeu du morpion aveugle.")
+print ("Pour jouer il suffit de choisir une case de 0 à 8, les cases sont numérotees de la maniere suivante :")
+print ("")
+print ("     0 1 2 ")
+print ("     3 4 5 ")
+print ("     6 7 8 ")
+print ("")
+
 # connexion au serveur
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("", 1111))
 
+case_number = raw_imput ("Choisissez une case a jouer : ")
